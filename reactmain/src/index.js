3733925c7aa8433dc7e262ca365b148d06a17b6e
Taavi <taavi247@gmail.com>
2022-11-02 15:36:50 +0200

    Project created with basic Django and React integration

---
+++ reactmain/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Nav from './Nav'
+
+const root = ReactDOM.createRoot(document.getElementById('reactmain'));
+root.render(
+    <React.StrictMode>
+        <Nav />
+    </React.StrictMode>
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+    name: 'reactmain-config',
+    entry: {
+        reactmain: './reactmain/src/index.js',
+    },
+    output: {
+        path: path.resolve('./reactmain/static/reactmain/'),
+        filename: 'index-bundle.js',
+        publicPath: 'static/reactmain/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                exclude: /(node_modules)/,
+                loader: 'file-loader',
+            },  
+        ],
+    },
+}
